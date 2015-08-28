75a2f4810a05d1d02923fb0857618bdd28324e3a
Harbon <harbon@lydiabox.com>
2015-08-28 13:41:27 +0800

    modepath

--- src/app/routes.rs
+++ src/app/routes.rs
@@ -4,7 +4,7 @@ use router::Router;
 
 pub fn all() -> Router {
 	let mut router = Router::new();
-    router.get("/", controllers::get_main);
+    router.get("/main", controllers::get_main);
     router.get("/docs", controllers::get_docs_list);
 	router.get("/musics", controllers::get_musics_list);
 	router.get("/pictures", controllers::get_pictures_list);
