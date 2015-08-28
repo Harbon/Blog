c5254732f62fcee50864e82e9d4a49667821c260
Harbon <harbon@lydiabox.com>
2015-08-28 14:56:01 +0800

    fix_bugs

--- src/app/helpers/template_engine_helper.rs
+++ src/app/helpers/template_engine_helper.rs
@@ -1,5 +1,6 @@
 use handlebars_iron::{Template, HandlebarsEngine};
 
 pub fn init_template_engine() -> HandlebarsEngine {
+    // HandlebarsEngine::new("/home/Harbon/Blog/src/templates", ".hbs")
     HandlebarsEngine::new("/home/Harbon/Blog/src/templates", ".hbs")
 }
